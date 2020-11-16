a7f2e6ba1326b95f471ffe32e420dfa5dba589dd
arifudin <nanangarif404@gmail.com>
2020-11-16 14:26:00 +0700

    tambah enter, tes commit

---
+++ app/src/main/java/com/example/stroll/database/DatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.example.stroll.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public static String DATABASE_NAME = "dbdestinasiapp";
+    private static final int DATABASE_VERSION = 1;
+    private static final String SQL_CREATE_TABLE_NOTE = String.format("CREATE TABLE %s"
+                    + " (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL)",
+            DatabaseContract.TABLE_NAME,
+            DatabaseContract.DestinasiColumns._ID,
+            DatabaseContract.DestinasiColumns.TITLE,
+            DatabaseContract.DestinasiColumns.DESCRIPTION,
+            DatabaseContract.DestinasiColumns.ADDRESS,
+            DatabaseContract.DestinasiColumns.PHOTO
+    );
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TABLE_NOTE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.TABLE_NAME);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/stroll/ui/saran/SaranFragment.java
+++ app/src/main/java/com/example/stroll/ui/saran/SaranFragment.java
@@ -24,7 +24,9 @@ public class SaranFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_saran, container, false);
     }
 }
