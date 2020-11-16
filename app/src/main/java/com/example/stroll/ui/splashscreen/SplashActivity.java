a6fb076ab58cbb14295ed54bd12d80b48a950ec2
anggerita <anggeritaj@gmail.com>
2020-11-16 15:05:39 +0700

    nambah splash screen

---
+++ app/src/main/java/com/example/stroll/ui/splashscreen/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.stroll.ui.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.stroll.R;
+import com.example.stroll.ui.home.HomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private int waktu_loading=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+
+    }
+}
