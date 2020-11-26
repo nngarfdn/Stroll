7638b6dce1107fd5d4b05c22bf41af84f2c50849
anggerita <anggeritaj@gmail.com>
2020-11-26 21:10:08 +0700

    nambah data pantai

--- app/src/main/java/com/example/stroll/utils/DataPantai.java
+++ app/src/main/java/com/example/stroll/utils/DataPantai.java
@@ -7,38 +7,43 @@ import java.util.ArrayList;
 public class DataPantai {
 
     private static int[] id = {
-            11,
-            12,
-            13,
-            14
+            01,
+            02,
+            03,
+            04,
+            05
     };
 
     private static String[] photoPantai = {
-            "https://cdn.rentalmobilbali.net/wp-content/uploads/2011/12/Pantai-Pasir-Putih-Di-Bali-1-692x376.jpg",
-            "https://asset.kompas.com/crops/ktWS7GA9Ah0iGVq26DEgWIiZwBQ=/158x105:1418x945/750x500/data/photo/2019/09/08/5d75120e0290f.jpeg",
-            "https://images.solopos.com/2019/12/pantai-kali-kencana-1200x675.jpg",
-           "https://www.itrip.id/wp-content/uploads/2020/04/Wisata-Pantai-Pangandaran.jpg"
+            "https://www.1001malam.com/travel/wp-content/uploads/2016/09/Sumber-zonalibur.jpg",
+            "https://www.alodiatour.com/wp-content/uploads/2018/01/wisata-pantai-kuwaru.jpg",
+            "https://www.pantainesia.com/wp-content/uploads/2018/03/Pantai-Baru-1248x703.jpg",
+            "https://jarwadi.files.wordpress.com/2016/05/tebing-laut-pecislaut-begah-gunungkidul.jpg?w=660",
+            "https://piknikasik.com/wp-content/uploads/2016/07/pantai-timang-gunungkidul-jogja1.jpg"
     };
 
     private static String[] namaPantai = {
-            "Pantai parangtritis",
+            "Pantai Parangtritis",
             "Pantai Kuwaru",
             "Pantai Baru",
-            "Pantai Lama"
+            "Laut Bekah",
+            "Pantai Timang"
     };
 
     private static String[] deskripsiPantai = {
-            "deskripsiPantai Pantai parangtritis",
-            "deskripsiPantai Pantai Kuwaru",
-            "deskripsiPantai Pantai Baru",
-            "deskripsiPantai Pantai Lama"
+            "Pantai Parangtritis adalah pantai yang paling populer yang terletak di Desa Parangtritis, Kecamatan Kretek, Kabupaten Bantul Yogyakarta",
+            "Pantai Kuwaru merupakan sebuah pantai yang di sekitarnya tumbuh banyak pohon cemara yang cukup rindang",
+            "Pantai Baru merupakan salah satu pantai di Jogja yang masih perawan sehingga relatif bersih, nyaman, dan terbebas dari polusi udara",
+            "Destinasi alam terpencil di Gunung Kidul yang menyuguhkan laut dan tebing",
+            "Pantai Timang Yogyakarta menawarkan aktvitas wisata yang menantang tidak seperti kebanyakan pantai"
     };
 
     private static String[] alamatPantai = {
-            "alamat Pantai parangtritis",
-            "alamat Pantai Kuwaru",
-            "alamat deskripsiPantai Pantai Baru",
-            "alamat deskripsiPantai Pantai Lama"
+            "Jl. Parangtritis Km. 28 Parangtritis Yogyakarta 55188",
+            "Dusun Poncosari, Srandakan, Kab. Bantul, Yogyakarta 55762",
+            "Ngentak, Poncosari, Srandakan, Bantul, daerah Istimewa Yogyakarta",
+            "Dusun Temon, Desa Giripurwo. Kecamatan Purwosari, Kabupaten Gunungkidul, DI Yogyakarta",
+            "Padukuhan Danggolo, Desa Purwodadi, Kecamatan Tepus, Kabupaten Gunung Kidul, Daerah Istimewa Yogyakarta"
     };
 
 
